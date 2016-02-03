08a8c56c0422b6d3e8a6adb68fdf4d19843cfa1d
michael.budnick <michael.budnick@outlook.com>
2016-02-02 22:13:20 -0500

    Torch class

--- torch.rb
+++ torch.rb
@@ -26,4 +26,13 @@ class Torch
   		# 	end
   		# end
   end
+
+  def inspect
+    lit ?  "By the looks of it, the torch will burn for (x more movements)" :
+          "A piece of wood with flammable resin inside.\nIt can be lit to provide a bright flame."
+  end
+
+  def to_s
+    lit ? "a bright flaming torch" : "a torch"
+  end
 end
