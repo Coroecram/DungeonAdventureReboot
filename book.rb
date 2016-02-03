7f19b39ac478c0686993d799c19f91ad3122cb88
michael.budnick <michael.budnick@outlook.com>
2016-02-02 21:45:00 -0500

    Create book class

--- book.rb
+++ book.rb
@@ -1,27 +1,29 @@
-def inspect_book
-	puts "___________________________________________________________________________".center(80)
-	puts "Hello #{@traveler}, looking for some help?\n".center(80)
-	puts "Here's a list of some handy commands and a short description of what they do!\n".center(80)
-	puts "Type [command] [item or direction] and press ENTER".center(80)
-	puts "Command \t Description"
-	puts "move/go   Move your character in [direction].\n"
-	puts "inspect   Inspect an [object].\n\t  Inspect your bag, hands, or the room to manipulate the items in them.\n"
-	puts "take/grab Pick up an [item]."
-	puts "put/place Put down an [item] in your hands."
-	puts "light \t  Light  the [object] on fire (be careful!)."
-	puts "clean \t  Some [items] are dirty.\n\n"
-	puts "____________________________________________________________________________".center(80)
-	puts "Press <ENTER> to look at the facing page."
-	gets
-	puts "____________________________________________________________________________".center(80)
-	puts "Here are some helpful tips #{@traveler}!".center(80)
-	puts "Don't forget torches burn out, it is not safe to be lost in the dark!".center(80)
-	puts "You will always drop and pickup the most burnt torch first.".center(80)
-	puts "Branches can be taken as torches.".center(80)
-	puts "Do not use descriptors except for a 'lit' torch or lamp.".center(80)
-	puts "Unique abbreviations are acceptable.".center(80)
-	puts "Again, don't forget torches burn out, it is not safe to be lost in the dark!".center(80)
-	puts "Check how long your lit torches will last frequently!".center(80)
-	puts "____________________________________________________________________________".center(80)
-	puts "Afraid to read on, you close the book."
+class Book
+  def inspect_book
+  	puts "___________________________________________________________________________".center(80)
+  	puts "Hello #{@traveler}, looking for some help?\n".center(80)
+  	puts "Here's a list of some handy commands and a short description of what they do!\n".center(80)
+  	puts "Type [command] [item or direction] and press ENTER".center(80)
+  	puts "Command \t Description"
+  	puts "move/go   Move your character in [direction].\n"
+  	puts "inspect   Inspect an [object].\n\t  Inspect your bag, hands, or the room to manipulate the items in them.\n"
+  	puts "take/grab Pick up an [item]."
+  	puts "put/place Put down an [item] in your hands."
+  	puts "light \t  Light  the [object] on fire (be careful!)."
+  	puts "clean \t  Some [items] are dirty.\n\n"
+  	puts "____________________________________________________________________________".center(80)
+  	puts "Press <ENTER> to look at the facing page."
+  	gets
+  	puts "____________________________________________________________________________".center(80)
+  	puts "Here are some helpful tips #{@traveler}!".center(80)
+  	puts "Don't forget torches burn out, it is not safe to be lost in the dark!".center(80)
+  	puts "You will always drop and pickup the most burnt torch first.".center(80)
+  	puts "Branches can be taken as torches.".center(80)
+  	puts "Do not use descriptors except for a 'lit' torch or lamp.".center(80)
+  	puts "Unique abbreviations are acceptable.".center(80)
+  	puts "Again, don't forget torches burn out, it is not safe to be lost in the dark!".center(80)
+  	puts "Check how long your lit torches will last frequently!".center(80)
+  	puts "____________________________________________________________________________".center(80)
+  	puts "Afraid to read on, you close the book."
+  end
 end
