973c50ad720d268331b639bae87e32550f804ad5
michael.budnick <michael.budnick@outlook.com>
2016-02-02 21:47:50 -0500

    Hand class

--- hand.rb
+++ hand.rb
@@ -1,14 +1,23 @@
-def in_hand
-	@scope_hands = 1; @scope_bag = 0; @scope_room = 0
-	if @in_hands.length == 0
-		puts "There is nothing in your hands."
-	else
-		things = []
-		@in_hands.each {|object| things << find_object((object[/\D+/]).to_sym).name}
-		if things.length == 1
-			puts "There is " + things[0] + " in your hand."
-		else
-			puts "There is " + things.join(" and ") + " in your hands."
-		end
-	end
+class Hand
+  attr_accessor :contents
+
+  def initialize(contents)
+    @contents = contents
+  end
+
+  def holding
+  	if @contents.length == 0
+  		puts "There is nothing in your hands."
+  	else
+  		if contents.length == 1
+  			puts "There is " + @contents[0] + " in your hand."
+  		else
+  			puts "There is " + @contents.join(" and ") + " in your hands."
+  		end
+  	end
+  end
+
+  def to_s
+    "Your good ole' reliable hands."
+  end
 end
