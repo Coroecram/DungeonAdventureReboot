d8df9848a8e06c40391bf710a606d3f303b13f28
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:18:24 -0500

    Computer spec

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,13 +2,13 @@ require 'computer'
 
 RSpec.describe Computer do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Computer.new.inspect).to eq(    "It's an old computer, still running, on top of the platform.")
+      it 'returns the computer description' do
+        expect(Computer.new.inspect).to eq("It's an old computer, still running, on top of the platform.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Computer.new.to_s).to eq(    "a computer")
+      it 'returns the computer string' do
+        expect(Computer.new.to_s).to eq("a computer")
       end
     end
 end
