e720ab236e8a5a9fb8eb579bf0dcdca0ded831b0
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:28:54 -0500

    Platform spec

--- spec/platform_spec.rb
+++ spec/platform_spec.rb
@@ -2,13 +2,13 @@ require 'platform'
 
 RSpec.describe Platform do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Platform.new.inspect).to eq(    "a stone platform with a fragment missing and a computer on top.")
+      it 'returns the platform description' do
+        expect(Platform.new.inspect).to eq("a stone platform with a fragment missing and a computer on top.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Platform.new.to_s).to eq(    "a stone platform")
+      it 'returns the platform string' do
+        expect(Platform.new.to_s).to eq("a stone platform")
       end
     end
 end
