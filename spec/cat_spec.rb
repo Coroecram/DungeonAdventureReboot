1e2fe58cceed1eca9351e4bfe31c7d56861a3c1f
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:16:16 -0500

    Cat spec

--- spec/cat_spec.rb
+++ spec/cat_spec.rb
@@ -2,12 +2,12 @@ require 'cat'
 
 RSpec.describe Cat do
     describe '#inspect' do
-      it 'returns the object description' do
+      it 'returns the cat description' do
         expect(Cat.new.inspect).to eq(    "It's a Siamese cat in the corner, looks friendly enough.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
+      it 'returns the cat string' do
         expect(Cat.new.to_s).to eq(    "a cat")
       end
     end
