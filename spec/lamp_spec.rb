0b753bd728d40d16d0ca1a5cd93a53930b8b5c85
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:27:17 -0500

    Fix lamp spec

--- spec/lamp_spec.rb
+++ spec/lamp_spec.rb
@@ -2,13 +2,13 @@ require 'lamp'
 
 RSpec.describe Lamp do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Lamp.new.inspect).to eq(    lit ? "A black metal lamp hanging on a post about 6 feet tall burning brightly." :)
+      it 'returns the lamp description' do
+        expect(Lamp.new.inspect).to eq("A black metal lamp hanging on a post about 6 feet tall in the middle of the room.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Lamp.new.to_s).to eq(    lit ? "a lamp burning brightly" : "a lamp")
+      it 'returns the lamp string' do
+        expect(Lamp.new.to_s).to eq("a lamp")
       end
     end
 end
