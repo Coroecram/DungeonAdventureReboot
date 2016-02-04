25b214e85366fdc5a48eea614cc4340ed6be50ef
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:16:42 -0500

    Bag spec

--- spec/bag_spec.rb
+++ spec/bag_spec.rb
@@ -2,12 +2,12 @@ require 'bag'
 
 RSpec.describe Bag do
     describe '#inspect' do
-      it 'returns the object description' do
+      it 'returns the bag description' do
         expect(Bag.new.inspect).to eq("A black leather-bound book with a certain feel. There are no markings on it.\nYou open to the first page.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
+      it 'returns the bag description' do
         expect(Bag.new.to_s).to eq("A worn black leather messenger bag, big enough to hold a few items.")
       end
     end
