ff4444353451832485306ff90a599f4dec42307f
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:20:59 -0500

    Disk spec

--- spec/disk_spec.rb
+++ spec/disk_spec.rb
@@ -2,13 +2,13 @@ require 'disk'
 
 RSpec.describe Disk do
     describe '#inspect' do
-      it 'returns the object description' do
+      it 'returns the disk description' do
         expect(Disk.new.inspect).to eq(    "An old-time floppy disk")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Disk.new.to_s).to eq(    "a floppy disk")
+      it 'returns the disk string' do
+        expect(Disk.new.to_s).to eq("a floppy disk")
       end
     end
 end
