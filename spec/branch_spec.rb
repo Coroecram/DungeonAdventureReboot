539a75553a96628ec056caa42d7e492db05bca85
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:15:55 -0500

    Branch spec

--- spec/branch_spec.rb
+++ spec/branch_spec.rb
@@ -2,13 +2,13 @@ require 'branch'
 
 RSpec.describe Branch do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Branch.new.inspect).to eq(    "A branch; it looks like it could be taken and used as a torch.")
+      it 'returns the branch description' do
+        expect(Branch.new.inspect).to eq("A branch; it looks like it could be taken and used as a torch.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Branch.new.to_s).to eq(    "a tree branch")
+      it 'returns the branch name' do
+        expect(Branch.new.to_s).to eq("a tree branch")
       end
     end
 end
