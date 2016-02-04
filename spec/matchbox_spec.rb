448ca52a1bb634c46a6168f340c3ae072f40e85c
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:28:04 -0500

    Monitor spec

--- spec/matchbox_spec.rb
+++ spec/matchbox_spec.rb
@@ -2,12 +2,12 @@ require 'matchbox'
 
 RSpec.describe Matchbox do
     describe '#inspect' do
-      it 'returns the object description' do
+      it 'returns the matchbox description' do
         expect(Matchbox.new.inspect).to eq("A strike-on-box with so many matches you can't run out.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
+      it 'returns the matchbox string' do
         expect(Matchbox.new.to_s).to eq("a matchbox")
       end
     end
--- spec/monitor_spec.rb
+++ spec/monitor_spec.rb
@@ -2,13 +2,13 @@ require 'monitor'
 
 RSpec.describe Monitor do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Monitor.new.inspect).to eq(    "A computer monitor on top of the computer. It is too dusty to read")
+      it 'returns the monitor description' do
+        expect(Monitor.new.inspect).to eq("A computer monitor on top of the computer. It is too dusty to read")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Monitor.new.to_s).to eq(    "a monitor")
+      it 'returns the monitor string' do
+        expect(Monitor.new.to_s).to eq("a monitor")
       end
     end
 end
