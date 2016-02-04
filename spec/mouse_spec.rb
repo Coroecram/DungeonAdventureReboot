a60c91c229ae9a3117827583e01a8f5cc6379cb6
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:28:27 -0500

    Mouse spec

--- spec/mouse_spec.rb
+++ spec/mouse_spec.rb
@@ -2,13 +2,13 @@ require 'mouse'
 
 RSpec.describe Mouse do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Mouse.new.inspect).to eq(    "A plain-old 2-button computer mouse.")
+      it 'returns the mouse description' do
+        expect(Mouse.new.inspect).to eq("A plain-old 2-button computer mouse.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Mouse.new.to_s).to eq(    "a mouse (computer)")
+      it 'returns the mouse string' do
+        expect(Mouse.new.to_s).to eq("a mouse (computer)")
       end
     end
 end
