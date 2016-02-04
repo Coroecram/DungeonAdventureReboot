2f8e108f400aae45d202f6c9918be7b48ed6c70e
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:22:36 -0500

    Hole spec

--- spec/hole_spec.rb
+++ spec/hole_spec.rb
@@ -2,12 +2,12 @@ require 'hole'
 
 RSpec.describe Hole do
     describe '#inspect' do
-      it 'returns the object description' do
+      it 'returns the hole description' do
         expect(Hole.new.inspect).to eq("There is a small hole in the wall. You can hear scurrying inside.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
+      it 'returns the hole string' do
         expect(Hole.new.to_s).to eq("a small hole in the wall")
       end
     end
