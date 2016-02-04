659a8d42b5b73953b400736e840c16395159e73a
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:31:05 -0500

    Tree spec

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -2,13 +2,13 @@ require 'tree'
 
 RSpec.describe Tree do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Tree.new.inspect).to eq(		"There is a large dead tree with many branches that look like they can burn.\nWho knows how it got here?")
+      it 'returns the tree description' do
+        expect(Tree.new.inspect).to eq("There is a large dead tree with many branches that look like they can burn.\nWho knows how it got here?")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Tree.new.to_s).to eq(		"a large dead tree")
+      it 'returns the tree string' do
+        expect(Tree.new.to_s).to eq("a large dead tree")
       end
     end
 end
