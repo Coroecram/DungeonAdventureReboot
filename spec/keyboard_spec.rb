f0e8cd1032879cd741a3747d13effe8e2020ab78
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:24:48 -0500

    Keyboard spec

--- spec/keyboard_spec.rb
+++ spec/keyboard_spec.rb
@@ -2,13 +2,13 @@ require 'keyboard'
 
 RSpec.describe Keyboard do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Keyboard.new.inspect).to eq(    "A standard QWERTY keyboard.")
+      it 'returns the keyboard description' do
+        expect(Keyboard.new.inspect).to eq("A standard QWERTY keyboard.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Keyboard.new.to_s).to eq(    "a computer keyboard")
+      it 'returns the keyboard string' do
+        expect(Keyboard.new.to_s).to eq("a computer keyboard")
       end
     end
 end
