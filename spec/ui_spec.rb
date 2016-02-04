6026759d2b8bc473f9af97bd99d2280ba0fbf3ba
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:31:20 -0500

    Clean UI spec

--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -1,14 +1,5 @@
 require 'ui'
 
 RSpec.describe UI do
-    describe '#inspect' do
-      it 'returns the object description' do
-        expect(UI.new.inspect).to eq()
-      end
-    end
-    describe '#to_s' do
-      it 'returns the object name' do
-        expect(UI.new.to_s).to eq()
-      end
-    end
+
 end
