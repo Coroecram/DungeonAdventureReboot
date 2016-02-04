91f3b9b86888a0673b5cebd3ff6d256d63525a98
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:21:27 -0500

    Fragment spec

--- spec/fragment_spec.rb
+++ spec/fragment_spec.rb
@@ -2,13 +2,13 @@ require 'fragment'
 
 RSpec.describe Fragment do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Fragment.new.inspect).to eq(		"A stone fragment with carving on it.")
+      it 'returns the fragment description' do
+        expect(Fragment.new.inspect).to eq("A stone fragment with carving on it.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Fragment.new.to_s).to eq(		"a stone fragment")
+      it 'returns the fragment string' do
+        expect(Fragment.new.to_s).to eq("a stone fragment")
       end
     end
 end
