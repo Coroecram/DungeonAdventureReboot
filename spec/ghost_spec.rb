e0d0f82a66bd199eb3c15b139e6d9f39e0c89b71
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:22:06 -0500

    Ghost spec

--- spec/ghost_spec.rb
+++ spec/ghost_spec.rb
@@ -2,13 +2,13 @@ require 'ghost'
 
 RSpec.describe Ghost do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Ghost.new.inspect).to eq(    "A terrifying ghost moaning and staring right at you!")
+      it 'returns the ghost description' do
+        expect(Ghost.new.inspect).to eq("A terrifying ghost moaning and staring right at you!")
       end
     end
     describe '#to_s' do
       it 'returns the object name' do
-        expect(Ghost.new.to_s).to eq(    spooky  ? "a spooky ghost" : "a ghost")
+        expect(Ghost.new.to_s).to eq("a spooky ghost")
       end
     end
 end
