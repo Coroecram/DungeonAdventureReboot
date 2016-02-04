a6bba6e642960b4d2ea989ca98d7050cd0ce28e9
michael.budnick <michael.budnick@outlook.com>
2016-02-03 22:30:44 -0500

    Fix torch spec

--- spec/torch_spec.rb
+++ spec/torch_spec.rb
@@ -2,13 +2,13 @@ require 'torch'
 
 RSpec.describe Torch do
     describe '#inspect' do
-      it 'returns the object description' do
-        expect(Torch.new.inspect).to eq(    lit ?  "By the looks of it, the torch will burn for (x more movements)" :)
+      it 'returns the torch description' do
+        expect(Torch.new.inspect).to eq("A piece of wood with flammable resin inside.\nIt can be lit to provide a bright flame.")
       end
     end
     describe '#to_s' do
-      it 'returns the object name' do
-        expect(Torch.new.to_s).to eq(    lit ? "a bright flaming torch" : "a torch")
+      it 'returns the torch string' do
+        expect(Torch.new.to_s).to eq("a torch")
       end
     end
 end
