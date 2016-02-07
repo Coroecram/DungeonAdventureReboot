ff6f143c492a0e3f08bc5c9eaadfcb9280512d62
michael.budnick <michael.budnick@outlook.com>
2016-02-06 23:15:50 -0500

    Inspect tests for chest

--- lib/chest.rb
+++ lib/chest.rb
@@ -1,19 +1,49 @@
+require 'keyboard'
+
 class Chest
-  attr_accessor :locked
+  attr_accessor :locked, :opened
 
   def initialize
     @locked = true
+    @opened = false
+    @contents = [Keyboard.new]
   end
 
   def unlock
     self.locked = false
   end
 
+  def open
+    self.opened = true if self.locked == false
+  end
+
+  def close
+    self.opened = !self.opened if self.opened
+  end
+
+  def empty
+    @contents = []
+  end
+
+  def empty?
+    self.locked ? false : @contents.empty?
+  end
+
   def inspect
-    return self.locked ? "A large locked wooden chest with a keypad on it." : "huh"
+    if self.locked
+      "A large locked wooden chest with a keypad on it."
+    else
+      if self.opened
+        self.empty? ?  "An open treasure chest with only dust inside." :
+                       "An open treasure chest with a keyboard inside."
+      else
+        self.empty? ? "A large unlocked treasure chest with only dust inside." :
+                      "A large unlocked wooden treasure chest."
+      end
+    end
   end
 
   def to_s
-    return self.locked ? "a large wooden treasure chest" : "not true"
+    return self.locked ? "a large wooden treasure chest" : ""
   end
 end
--- spec/chest_spec.rb
+++ spec/chest_spec.rb
@@ -4,25 +4,81 @@ RSpec.describe Chest do
   before(:each) do
     @chest = Chest.new
   end
+
     describe '@locked' do
       it 'is locked when initialized' do
         expect(@chest.locked).to eq(true)
       end
     end
+
     describe '#unlock' do
       it 'can be unlocked' do
         @chest.unlock
         expect(@chest.locked).to eq(false)
       end
     end
-    describe '#inspect' do
-      it 'returns the object description' do
-        expect(@chest.inspect).to eq("A large locked wooden chest with a keypad on it.")
+
+    describe '#open' do
+      it 'can be opened' do
+        @chest.unlock
+        @chest.open
+        expect(@chest.open).to eq(true)
       end
     end
+
+    describe '#inspect' do
+      it 'returns the locked chest description' do
+        expect(@chest.inspect).to eq("A large locked wooden chest with a keypad on it.")
+      end
+
+      it 'returns the unlocked chest description' do
+        test_unlock
+        expect(@chest.opened).to eq(false)
+      end
+
+      it 'returns the unlocked and open chest description' do
+        unlock_and_open
+        expect(@chest.inspect).to eq("An open treasure chest with a keyboard inside.")
+      end
+
+      it 'returns the unlocked, open, and empty chest description' do
+        test_empty
+        expect(@chest.inspect).to eq("An open treasure chest with only dust inside.")
+      end
+
+      it 'returns the unlocked, empty, and closed chest description' do
+        empty_and_close
+        expect(@chest.inspect).to eq("A large unlocked treasure chest with only dust inside.")
+      end
+    end
+
     describe '#to_s' do
-      it 'returns the chest string' do
+      it 'returns the locked chest string' do
         expect(@chest.to_s).to eq("a large wooden treasure chest")
       end
     end
+end
+
+def test_unlock
+  @chest.unlock
+end
+
+def unlock_and_open
+  test_unlock
+  @chest.open
+end
+
+def test_close
+  unlock_and_open
+  @chest.close
+end
+
+def test_empty
+  unlock_and_open
+  @chest.empty
+end
+
+def empty_and_close
+  test_empty
+  @chest.close
 end
