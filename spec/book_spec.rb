7d95301f8f9fb5d95a3ddadbbfd11e7ca20e96a6
michael.budnick <michael.budnick@outlook.com>
2016-02-06 21:39:30 -0500

    Book before hook

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,14 +1,17 @@
 require 'book'
 
 RSpec.describe Book do
+  before(:all) do
+    @book = Book.new
+  end
     describe '#inspect' do
       it 'returns the book description' do
-        expect(Book.new.inspect).to eq("A black leather-bound book with a certain feel. There are no markings on it.\nYou open to the first page.")
+        expect(@book.inspect).to eq("A black leather-bound book with a certain feel. There are no markings on it.\nYou open to the first page.")
       end
     end
     describe '#to_s' do
       it 'returns the book string' do
-        expect(Book.new.to_s).to eq("a magical book")
+        expect(@book.to_s).to eq("a magical book")
       end
     end
 end
